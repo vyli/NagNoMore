9f33a3598164bb600abef55483c32431396540ef
Hanna <hapaloma@yahoo.com>
2016-07-08 14:05:01 +0300

    First version of TaskController for react. Includes just basic Crud+List.

--- NagNoMore/src/main/java/edu/uta/sis/nagnomore/web/tasks/ReactTasksController.java
+++ NagNoMore/src/main/java/edu/uta/sis/nagnomore/web/tasks/ReactTasksController.java
@@ -4,14 +4,17 @@ import edu.uta.sis.nagnomore.domain.data.Task;
 import edu.uta.sis.nagnomore.domain.service.TaskService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
  * Created by Hanna on 7.7.2016.
  */
+@Controller
 public class ReactTasksController {
 
     @Autowired
@@ -19,10 +22,78 @@ public class ReactTasksController {
 
     Logger logger = Logger.getLogger(this.getClass().getName());
 
-    @RequestMapping("/tasks/react")
+    @RequestMapping("/react/tasks")
     public @ResponseBody
     List<Task> listTasks(){
 
         return taskService.findAll();
     }
+    @RequestMapping (value = "/react/tasks/create", method = RequestMethod.GET)
+    @ResponseBody
+    public Task create(){
+        logger.debug("Create Task - Get - react");
+        Task task = new Task();
+        return task;
+    }
+
+    //in case of Task binding result errors return false
+    // TODO: ?add @RequestParam infront of Task ?
+    @RequestMapping(value = "/react/tasks/create", method = RequestMethod.POST)
+    @ResponseBody
+    public boolean create(@Valid Task task, BindingResult bindingResult){
+        logger.debug("Create Task - Post - react");
+        if(bindingResult.hasErrors()){
+            logger.debug("Create Task bindingResult.hasErrors - react");
+            return false;
+        }
+        taskService.addTask(task);
+        return true;
+    }
+
+    @RequestMapping(value = "/react/tasks/update/{id}",method = RequestMethod.GET)
+    @ResponseBody
+    public Task update(@PathVariable("id") Integer id){
+        logger.debug("Update GET - react Task " + id);
+        Task task = taskService.find(id);
+        return task;
+    }
+
+    //in case of Task binding result errors return false
+    // TODO: ?add @RequestParam infront of Task ?
+    @RequestMapping(value = "/react/tasks/update/{id}", method = RequestMethod.POST)
+    @ResponseBody
+    public boolean update(@PathVariable("id") Integer id, @Valid Task task, BindingResult bindingResult){
+        logger.debug("Update POST -react Task " + id);
+        if(bindingResult.hasErrors()){
+            logger.debug("Update POST Task " + id + "bindingResult.hasErrors");
+            return false;
+        }
+        task.setId(id);
+        taskService.updateTask(task);
+        return true;
+    }
+
+
+    @RequestMapping(value = "/react/tasks/remove/{id}",method = RequestMethod.GET)
+    @ResponseBody
+    public void remove(@PathVariable ("id") Integer id){
+        logger.debug("Remove - react - Task " + id);
+        taskService.remove(taskService.find(id));
+        return;
+    }
+
+
+    @RequestMapping(value = "/react/tasks/read/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Task read(@PathVariable("id") Integer id){
+        logger.debug("Read - react - Task " + id);
+        Task task = taskService.find(id);
+        return task;
+    }
 }
+/*
+
+
+
+
+*/
